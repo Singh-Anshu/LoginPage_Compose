f1480df3af085458565cd1a79c45239e022878e0
Anshu Singh <anshusingh7738@gmail.com>
2024-03-04 00:18:08 +0530

    UPDATE: Creating SignUp Screen and working their components

--- app/src/main/java/com/example/loginpagecompose/components/Appcomponents.kt
+++ app/src/main/java/com/example/loginpagecompose/components/Appcomponents.kt
@@ -5,22 +5,48 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.loginpagecompose.R
 
 @Composable
 fun NormalTextComponent(textName: String) {
 
     Text(
         text = textName,
-        modifier = Modifier.fillMaxWidth().heightIn(min =  80.dp),
+        modifier = Modifier.fillMaxWidth().heightIn(min =  40.dp),
         style = TextStyle(
             fontWeight = FontWeight.Normal,
-            fontSize = 18.sp,
+            fontSize = 24.sp,
             fontStyle = FontStyle.Normal
-        )
+        ),
+        color = colorResource(id = R.color.colorText),
+        textAlign = TextAlign.Center
     )
+}
+
+@Composable
+fun HeadingTextComponent(textName: String) {
+
+    Text(
+        text = textName,
+        modifier = Modifier.fillMaxWidth().heightIn(),
+        style = TextStyle(
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            fontStyle = FontStyle.Normal
+        ),
+        color = colorResource(id = R.color.colorText),
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun MyTextFeild(){
+
 }
--- app/src/main/java/com/example/loginpagecompose/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/loginpagecompose/screens/SignUpScreen.kt
@@ -1,30 +1,47 @@
 package com.example.loginpagecompose.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.loginpagecompose.R
+import com.example.loginpagecompose.components.HeadingTextComponent
 import com.example.loginpagecompose.components.NormalTextComponent
 
 @Composable
 fun SignUpScreen() {
 
     Surface(
-        color = Color.White,
+
         modifier = Modifier.fillMaxSize()
+            .background(Color.White)
+            .padding(20.dp)
     ) {
 
-        NormalTextComponent(stringResource(R.string.hello))
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            NormalTextComponent(stringResource(R.string.hello))
+           // Spacer(Modifier.height(20.dp))
+            HeadingTextComponent(stringResource(R.string.create_an_account))
+
+        }
+
     }
 }
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun DefaultPreviewOfSignupScreen(){
+fun DefaultPreviewOfSignupScreen() {
 
     SignUpScreen()
 }
