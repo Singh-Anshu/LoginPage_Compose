75f84073d535c0671daa3fa569c4ec079b03b59d
Anshu Singh <anshusingh7738@gmail.com>
2024-03-06 12:44:36 +0530

    update: Created login screen and navigated to register and register screen to login screen.

--- app/src/main/java/com/example/loginpagecompose/screens/LoginScreen.kt
+++ app/src/main/java/com/example/loginpagecompose/screens/LoginScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,7 +22,6 @@ import androidx.compose.ui.unit.dp
 import com.example.loginpagecompose.R
 import com.example.loginpagecompose.app.Screen
 import com.example.loginpagecompose.components.ButtonComponent
-import com.example.loginpagecompose.components.CheckBoxComponent
 import com.example.loginpagecompose.components.ClickableLoginTextCompose
 import com.example.loginpagecompose.components.DividerComponent
 import com.example.loginpagecompose.components.HeadingTextComponent
@@ -31,10 +29,12 @@ import com.example.loginpagecompose.components.LoginByGoogleAndFB
 import com.example.loginpagecompose.components.MyTextField
 import com.example.loginpagecompose.components.NormalTextComponent
 import com.example.loginpagecompose.components.PassWordTextField
+import com.example.loginpagecompose.components.UnderLinedTextComponent
 import com.example.loginpagecompose.navigation.PostOfficeAppRouter
+import com.example.loginpagecompose.navigation.SystemBackButtonHandler
 
 @Composable
-fun SignUpScreen() {
+fun LoginScreen() {
 
     Surface(
         modifier = Modifier
@@ -43,38 +43,39 @@ fun SignUpScreen() {
             .padding(20.dp)
     ) {
 
-        val scrollSate  = rememberScrollState()
+        SystemBackButtonHandler {
+            PostOfficeAppRouter.navigateTo(Screen.SignUpScreen)
+        }
+
+        val scrollSate = rememberScrollState()
         Column(
-            modifier = Modifier.fillMaxSize().
-            verticalScroll(state = scrollSate),
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(state = scrollSate),
             horizontalAlignment = Alignment.CenterHorizontally,
 
-        ) {
+            ) {
+
             NormalTextComponent(stringResource(R.string.hello))
-           // Spacer(Modifier.height(20.dp))
-            HeadingTextComponent(stringResource(R.string.create_an_account))
+            HeadingTextComponent(stringResource(R.string.welcome_back))
             Spacer(Modifier.height(20.dp))
-            MyTextField(stringResource(R.string.first_name), Icons.Default.Person)
-            Spacer(Modifier.height(10.dp))
-            MyTextField(stringResource(R.string.last_name), Icons.Default.Person)
-            Spacer(Modifier.height(10.dp))
             MyTextField(stringResource(R.string.email), Icons.Default.Email)
             Spacer(Modifier.height(10.dp))
             PassWordTextField(stringResource(R.string.password), Icons.Default.Lock)
-            CheckBoxComponent(value = stringResource(R.string.terms_conditions),
-                onTexSelected = {
-                    PostOfficeAppRouter.navigateTo(Screen.TermsAndConditionsScreen)
-                })
+            Spacer(Modifier.height(20.dp))
+            UnderLinedTextComponent(textName = stringResource(id = R.string.forgot_password))
             Spacer(Modifier.height(80.dp))
-            ButtonComponent(value = stringResource(R.string.register))
+            ButtonComponent(value = stringResource(R.string.login))
             Spacer(Modifier.height(20.dp))
             DividerComponent()
             Spacer(Modifier.height(10.dp))
             LoginByGoogleAndFB()
             Spacer(Modifier.height(100.dp))
-            ClickableLoginTextCompose(tryingToLogin = true, onTexSelected = {
-                PostOfficeAppRouter.navigateTo(Screen.LoginScreen)
+            ClickableLoginTextCompose(tryingToLogin = false, onTexSelected = {
+                PostOfficeAppRouter.navigateTo(Screen.SignUpScreen)
+
             })
+
 
         }
 
@@ -83,7 +84,6 @@ fun SignUpScreen() {
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun DefaultPreviewOfSignupScreen() {
-
-    SignUpScreen()
+fun LoginScreenPreview() {
+    LoginScreen()
 }
